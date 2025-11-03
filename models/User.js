ddf57be470a52c70b7128b089a02d0f0b6b036e7
Olorunfemi Sunday <105191911+OlorunfemiSunday@users.noreply.github.com>
2025-11-03 23:25:32 +0100

    updated

--- models/User.js
+++ models/User.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    email: { 
-      type: String, 
-      required: true, 
-      unique: true, 
-      lowercase: true, 
-      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"] 
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
     },
     password: { type: String, required: true, minlength: 6 },
   },
